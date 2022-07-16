064d6365000edd86e2a21712aae0071d9d29e031
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-07-16 19:46:50 -0400

    basic player story

---
+++ Assets/Scripts/SceneManager/TransitionDoor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionDoor : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Next Scene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
