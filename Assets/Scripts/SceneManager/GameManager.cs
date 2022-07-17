781c96a6ab5ed6cc63f1ac62ed107aafcc4c182f
Andy Qin <qin.andrew@wustl.edu>
2022-07-17 08:18:58 -0500

    really bare bones game manager

--- Assets/Scripts/SceneManager/GameManager.cs
+++ Assets/Scripts/SceneManager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,68 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public static GameObject Manager;
     public static bool gameIsPaused;
+<<<<<<< HEAD
     public delegate void Health(int amount);
     public static event Health UpdateHealth;
 
+=======
+    public static event Action<GameState> OnGameStateChanged;
+    public GameState gameState;
+
+    public void UpdateGameState(GameState newState)
+    {
+        gameState = newState;
+        switch (newState)
+        {
+            case GameState.mainMenu:
+                HandleMainMenu();
+                break;
+            case GameState.rollingDice:
+                HandleRollingDice();
+                break;
+            case GameState.levelOne:
+                HandleLevelOne();
+                break;
+            case GameState.levelTwo:
+                HandleLevelTwo();
+                break;
+            case GameState.levelThree:
+                HandleLevelThree();
+                break;
+            case GameState.victory:
+                HandleVictory();
+                break;
+            case GameState.death:
+                HandleDeath();
+                break;
+            case GameState.pause:
+                HandlePause();
+                break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    
+
+    public void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    private void Start()
+    {
+        UpdateGameState(GameState.mainMenu);
+    }
+>>>>>>> b18482e (really bare bones game manager)
     // Start is called before the first frame update
     void Awake() {
         print("GameManager Awake");
@@ -33,15 +93,56 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void PauseGame ()
+
+    private void HandleMainMenu()
     {
-        if(gameIsPaused)
-        {
-            Time.timeScale = 0f;
-        }
-        else 
-        {
-            Time.timeScale = 1;
-        }
+
     }
+
+    private void HandleRollingDice()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandlePause()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleDeath()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleVictory()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleLevelThree()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleLevelTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleLevelOne()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum GameState
+{
+    mainMenu,
+    rollingDice,
+    levelOne,
+    levelTwo,
+    levelThree,
+    victory,
+    death,
+    pause,
 }
